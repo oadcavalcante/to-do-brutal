068d3fe9067d77f7061faddd4c87906ccba4aa39
oadcavalcante <aderc19@gmail.com>
2025-10-27 13:27:16 -0300

    :sparkles: feat: implementação no app.config.ts para facilitar a troca pro backend futuramente.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { TodosRepository } from './core/ports/todos.repository';
+import { LocalStorageTodosRepository } from './data/local-storage-todos.repository';
+// import { HttpTodosRepository } from './data/http-todos.repository'; // quando for usar backend
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    // ===== Troque aqui quando ligar o backend =====
+    { provide: TodosRepository, useExisting: LocalStorageTodosRepository },
+    // { provide: TodosRepository, useExisting: HttpTodosRepository },
+  ],
 };
